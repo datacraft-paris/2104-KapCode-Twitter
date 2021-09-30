09c5a21835e4b44d3699d9c661a7d471670f0ff6
ygirard <ygirard@nexway.com>
2021-09-30 12:31:02 +0200

    adding some geoloc info

--- _lib/mshTwitterUtils/__init__.py
+++ _lib/mshTwitterUtils/__init__.py
@@ -9,12 +9,69 @@ from nltk.stem import WordNetLemmatizer
 # ML-NLP
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification, pipeline
 import tensorflow as tf
+
 assert tf.__version__ >= "2.0"
+from time import sleep
+from geopy.geocoders import Nominatim
 
 # model used initialized at start
 tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine", use_fast=True)
 model = TFAutoModelForSequenceClassification.from_pretrained("tblard/tf-allocine")
 nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+
+def get_geolocation_information(place_name, user_agent="datacrafting_exploTweets", geocoder=None, sleep_time=None):
+    """ returns geolocation information using information from geopy
+    for a lambda or batch use , it is recommanded to passe the geocoder defined outside the loop to reduce the network requests"""
+    if place_name is None:
+        return pd.Series({
+            'address': '',
+            'altitude': 0,
+            'latitude': 0,
+            'longitude': 0
+        }
+        )
+    if geocoder is None:
+        geocoder = Nominatim(user_agent=user_agent)
+        location_info = geocoder.geocode(place_name)
+    if sleep_time is not None:
+        sleep(sleep_time)
+    location_info_series = pd.Series({
+        'address': location_info.address,
+        'altitude': location_info.altitude,
+        'latitude': location_info.latitude,
+        'longitude': location_info.longitude
+    }
+    )
+    return location_info_series
+
+
+def add_location_info(
+        data,
+        location_name='place_name',
+        user_agent="datacrafting_exploTweets",
+        geocoder=None,
+        sleep_time=None
+):
+    kut.display_message('adding geolocation information')
+    start = kut.yet()
+    if geocoder is None:
+        geocoder = Nominatim(user_agent=user_agent)
+    data_out = data.merge(
+        data.apply(
+            lambda row: get_geolocation_information(
+                place_name=row[location_name],
+                user_agent=user_agent,
+                geocoder=geocoder,
+                sleep_time=sleep_time
+            ),
+            axis=1
+        ),
+        left_index=True,
+        right_index=True
+    )
+    kut.job_done(start=start)
+    return data_out
 
 
 def load_join_data(
@@ -60,12 +117,12 @@ def clean_tweets(data, tweet_column='tweet', method=lemmatized_cleaning, clean_c
     return data
 
 
-def add_sentiment_information(data,lemme_column='lemmatizedTweet'):
-    kut.display_message('adding sentiment information based on '+ lemme_column)
+def add_sentiment_information(data, lemme_column='lemmatizedTweet'):
+    kut.display_message('adding sentiment information based on ' + lemme_column)
     print('expected time is 300s/1000rows')
-    start=kut.yet()
-    lemme_array=data[lemme_column].tolist()
-    nlp_df=pd.DataFrame(nlp(lemme_array)).rename(columns={'label':'sentiment_label','score':'sentiment_score'})
-    out=data.join(pd.DataFrame(nlp(lemme_array)))
+    start = kut.yet()
+    lemme_array = data[lemme_column].tolist()
+    nlp_df = pd.DataFrame(nlp(lemme_array)).rename(columns={'label': 'sentiment_label', 'score': 'sentiment_score'})
+    out = data.join(pd.DataFrame(nlp(lemme_array)))
     kut.job_done(start=start)
     return out
